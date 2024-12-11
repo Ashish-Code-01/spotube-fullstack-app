1ecd003b812bdd3872ec2f0c3fa5803f3fb6978e
Code-With-Aashish <tanujgaming87@gmail.com>
2024-12-11 10:16:52 +0530

    added the welcome page and the top pahe

--- spotube/app/index.tsx
+++ spotube/app/index.tsx
@@ -1,33 +1,80 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet, Image } from 'react-native';
 import { useRouter } from 'expo-router';
+import AppLogo from "../assets/images/applogo.png"
 
 const Welcome = () => {
     const router = useRouter();
     const [loading, setLoading] = useState(false);
 
-    const toNextPage = () => {
+    const toNextPage = useCallback(() => {
         setLoading(true);
         setTimeout(() => {
             router.replace('/otp');
             setLoading(false);
         }, 5000);
-    };
+    }, [router]);
 
     return (
-        <View>
+        <View style={styles.container}>
             {loading ? (
-                <Text>Loading...</Text>
+                <ActivityIndicator size="large" color="black" />
             ) : (
                 <>
-                    <Text>welcome</Text>
-                    <TouchableOpacity onPress={toNextPage}>
-                        <Text>next</Text>
+                    <Image source={AppLogo} style={styles.logoIMG} />
+                    <Text style={styles.title}>Welcome to Spotube Music App</Text>
+                    <Text style={styles.subtitle}>
+                        This app doesn't contain ads or advertisements.
+                    </Text>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={toNextPage}
+                        accessibilityLabel="Continue to the next page"
+                    >
+                        <Text style={styles.buttonText}>Continue</Text>
                     </TouchableOpacity>
                 </>
             )}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#558F22',
+    },
+    logoIMG: {
+        height: 250,
+        width: 350,
+        borderRadius: 20,
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: 20,
+        marginBottom: 24,
+        textAlign: 'center',
+        color: 'black',
+    },
+    button: {
+        backgroundColor: '#6200EE',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 20,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+});
 
 export default Welcome;
--- spotube/app/otp.tsx
+++ spotube/app/otp.tsx
@@ -1,12 +1,251 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Linking,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
+    ActivityIndicator,
+    Alert,
+} from 'react-native';
+import MaskInput from 'react-native-mask-input';
+import { isClerkAPIResponseError, useSignIn, useSignUp } from '@clerk/clerk-expo';
 
-const pageOtp = () => {
+const Colors = {
+    primary: '#25D366',
+    gray: '#808080',
+    lightGray: '#E0E0E0',
+    background: '#F5F5F5',
+};
+
+const GER_PHONE = [
+    '+',
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+];
+
+const Page = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const { signUp, setActive } = useSignUp();
+    const { signIn } = useSignIn();
+
+    const openLink = () => {
+        Linking.openURL('https://galaxies.dev');
+    };
+
+    const sendOTP = async () => {
+        if (phoneNumber.length < 12) {
+            Alert.alert('Invalid Phone Number', 'Please enter a valid phone number.');
+            return;
+        }
+
+        console.log('sendOTP', phoneNumber);
+        setLoading(true);
+
+        try {
+            await signUp!.create({ phoneNumber });
+
+            await signUp!.preparePhoneNumberVerification();
+
+            router.push(`/verify/${phoneNumber}`);
+        } catch (err) {
+            console.log('error', JSON.stringify(err, null, 2));
+
+            if (isClerkAPIResponseError(err)) {
+                if (err.errors[0].code === 'form_identifier_exists') {
+                    // User signed up before
+                    await trySignIn();
+                } else {
+                    Alert.alert('Error', err.errors[0].message);
+                }
+            } else {
+                Alert.alert('Error', 'An unexpected error occurred.');
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const trySignIn = async () => {
+        try {
+            const { supportedFirstFactors } = await signIn!.create({ identifier: phoneNumber });
+
+            const firstPhoneFactor: any = supportedFirstFactors.find((factor: any) => {
+                return factor.strategy === 'phone_code';
+            });
+
+            const { phoneNumberId } = firstPhoneFactor;
+
+            await signIn!.prepareFirstFactor({
+                strategy: 'phone_code',
+                phoneNumberId,
+            });
+
+            router.push(`/verify/${phoneNumber}?signin=true`);
+        } catch (error) {
+            console.error('Sign-in error:', error);
+            Alert.alert('Error', 'Failed to sign in. Please try again.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <View>
-            <Text>pageOtp</Text>
-        </View>
-    )
-}
+        <KeyboardAvoidingView
+            keyboardVerticalOffset={keyboardVerticalOffset}
+            style={{ flex: 1 }}
+            behavior="padding">
+            {loading && (
+                <View style={[StyleSheet.absoluteFill, styles.loading]}>
+                    <ActivityIndicator size="large" color={Colors.primary} />
+                    <Text style={{ fontSize: 18, padding: 10 }}>Sending code...</Text>
+                </View>
+            )}
 
-export default pageOtp
+            <View style={styles.container}>
+                <Text style={styles.description}>
+                    WhatsApp will need to verify your account. Carrier charges may apply.
+                </Text>
+
+                <View style={styles.list}>
+                    <View style={styles.listItem}>
+                        <Text style={styles.listItemText}>Germany</Text>
+                        <Ionicons name="chevron-forward" size={20} color={'gray'} />
+                    </View>
+                    <View style={styles.separator} />
+
+                    <MaskInput
+                        value={phoneNumber}
+                        keyboardType="numeric"
+                        autoFocus
+                        placeholder="+12 your phone number"
+                        onChangeText={(masked, unmasked) => {
+                            setPhoneNumber(masked);
+                        }}
+                        mask={GER_PHONE}
+                        style={styles.input}
+                    />
+                </View>
+
+                <Text style={styles.legal}>
+                    You must be{' '}
+                    <Text style={styles.link} onPress={openLink}>
+                        at least 16 years old
+                    </Text>{' '}
+                    to register. Learn how WhatsApp works with the{' '}
+                    <Text style={styles.link} onPress={openLink}>
+                        Meta Companies
+                    </Text>
+                    .
+                </Text>
+
+                <View style={{ flex: 1 }} />
+
+                <TouchableOpacity
+                    style={[styles.button, phoneNumber !== '' ? styles.enabled : null, { marginBottom: 20 }]}
+                    onPress={sendOTP}
+                    disabled={loading || phoneNumber === ''}>
+                    <Text style={[styles.buttonText, phoneNumber !== '' ? styles.enabledText : null]}>Next</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: 'red',
+        gap: 20,
+    },
+    description: {
+        fontSize: 14,
+        color: Colors.gray,
+    },
+    legal: {
+        fontSize: 12,
+        textAlign: 'center',
+        color: '#000',
+    },
+    link: {
+        color: 'green',
+    },
+    button: {
+        width: '100%',
+        alignItems: 'center',
+        backgroundColor: 'lightGray',
+        padding: 10,
+        borderRadius: 10,
+    },
+    enabled: {
+        backgroundColor: 'green',
+    },
+    enabledText: {
+        color: '#FFF',
+    },
+    buttonText: {
+        color: 'gray',
+        fontSize: 22,
+        fontWeight: '500',
+    },
+    list: {
+        backgroundColor: '#fff',
+        width: '100%',
+        borderRadius: 10,
+        padding: 10,
+    },
+    listItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 6,
+        marginBottom: 10,
+    },
+    listItemText: {
+        fontSize: 18,
+        color: 'green',
+    },
+    separator: {
+        width: '100%',
+        height: 1,
+        backgroundColor: 'gray',
+        opacity: 0.2,
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: '100%',
+        fontSize: 16,
+        padding: 6,
+        marginTop: 10,
+    },
+    loading: {
+        zIndex: 10,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default Page;
