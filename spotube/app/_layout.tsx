18069da4345cab85d42fa93770a66d2130024ef5
Code-With-Aashish <tanujgaming87@gmail.com>
2024-12-17 09:31:34 +0530

    added the file structure

--- spotube/app/_layout.tsx
+++ spotube/app/_layout.tsx
@@ -1,9 +1,13 @@
+import { ClerkProvider, ClerkLoaded, useAuth } from '@clerk/clerk-expo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, useSegments } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import * as SecureStore from 'expo-secure-store'
+import { TokenCache } from '@clerk/clerk-expo/dist/cache'
+
 
 
 export {
@@ -12,18 +16,44 @@ export {
 
 SplashScreen.preventAutoHideAsync();
 
-function RootLayoutNav() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="otp" options={{ headerTitle: "Enter your mobile Number" }} />
-      <Stack.Screen name="auth/[phone]" options={{ headerTitle: "Verify Your Number" }} />
-    </Stack>
-  );
+
+const publishableKey = 'pk_test_cmVmaW5lZC1ib3hlci01OS5jbGVyay5hY2NvdW50cy5kZXYk';
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+
+const createTokenCache = (): TokenCache => {
+  return {
+    getToken: async (key: string) => {
+      try {
+        const item = await SecureStore.getItemAsync(key)
+        if (item) {
+          console.log(`${key} was used 🔐 \n`)
+        } else {
+          console.log('No values stored under key: ' + key)
+        }
+        return item
+      } catch (error) {
+        console.error('secure store get item error: ', error)
+        await SecureStore.deleteItemAsync(key)
+        return null
+      }
+    },
+    saveToken: (key: string, token: string) => {
+      return SecureStore.setItemAsync(key, token)
+    },
+  }
 }
 
 
-export default function RootLayout() {
+
+function RootLayoutNav() {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
@@ -40,9 +70,40 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === 'auth';
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace('/(tabs)/index');
+    } else if (!isSignedIn) {
+      router.replace('/');
+    }
+  }, [isSignedIn]);
+
   if (!loaded) {
     return null;
   }
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="otp" options={{ headerTitle: "Enter your mobile Number" }} />
+      <Stack.Screen name="auth/[phone]" options={{ headerTitle: "Verify Your Number" }} />
+      <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
 
-  return <RootLayoutNav />;
+
+export default function RootLayout() {
+
+
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <RootLayoutNav />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
